872a7e08a46dadee37d177b1b5f6edafbecf2eca
Yuao <yai104.aya@gmail.com>
2025-02-23 17:08:10 -0500

    init3

---
+++ MusicPlayer/AppEnter.swift
@@ -0,0 +1,19 @@
+//
+//  AppEnter.swift
+//  MusicPlayer
+//
+//  Created by yuao ai on 2/23/25.
+//  Copyright © 2025 London App Brewery. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
+
--- MusicPlayer/ToneView.swift
+++ MusicPlayer/ToneView.swift
@@ -2,16 +2,18 @@ import SwiftUI
 import AVFoundation
 
 struct ToneView: View {
+    @Environment(\.presentationMode) var presentationMode
     @State private var audioPlayer: AVAudioPlayer?
-    let toneProcessor = ToneProcessor();
+    let toneProcessor = ToneProcessor()
     
     /// 根据传入的 note 名称和颜色生成一个按钮
     func noteButton(note: String, color: Color) -> some View {
         Button(action: {
             toneProcessor.playSound(note: note)
-                print(ToneProcessor.numToTone[Int(note)!]!)
-            }) {
-            // 空文本充当按钮占位，充满整个区域
+            if let intNote = Int(note), let tone = ToneProcessor.numToTone[intNote] {
+                print(tone)
+            }
+        }) {
             Text("")
                 .frame(maxWidth: .infinity)
                 .frame(height: 100)
@@ -20,23 +22,38 @@ struct ToneView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            noteButton(note: "1", color: .red)
-            noteButton(note: "2", color: .orange)
-            noteButton(note: "3", color: .yellow)
-            noteButton(note: "4", color: .green)
-            noteButton(note: "5", color: .blue)
-            noteButton(note: "6", color: .indigo)
-            noteButton(note: "7", color: .purple)
+        ZStack(alignment: .topLeading) {
+            VStack(spacing: 0) {
+                noteButton(note: "1", color: .red)
+                noteButton(note: "2", color: .orange)
+                noteButton(note: "3", color: .yellow)
+                noteButton(note: "4", color: .green)
+                noteButton(note: "5", color: .blue)
+                noteButton(note: "6", color: .indigo)
+                noteButton(note: "7", color: .purple)
+            }
+            .edgesIgnoringSafeArea(.all)
+            
+            // 自定义 Back 按钮，位于左上角
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Back")
+                    .padding(10)
+                    .background(Color.gray.opacity(0.7))
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding([.top, .leading], 20)
         }
-        .edgesIgnoringSafeArea(.all)
         .navigationBarHidden(true)
-
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ToneView()
+        NavigationView {
+            ToneView()
+        }
     }
 }
